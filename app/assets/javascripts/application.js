d6094f2ef4547055088bd0fcadd739255b15dc9b
hungmi <hungmihungmi@gmail.com>
2015-07-12 23:34:56 +0800

    added embed image slider and fade, randomly selected with js.
    TODO:
    	background-image-carousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,41 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var randomEffect = function(id, effect1, effect2) {
+  a = Math.random();
+  console.log(id + ':' + a);
+  if( a > 0.5) {
+    $('#' + id).removeClass(effect2);
+    $('#' + id).addClass(effect1);
+  } else {
+    $('#' + id).removeClass(effect1);
+    $('#' + id).addClass(effect2);
+  };
+}
+
+$(function(){
+  for (var i = 1; i < 6; i++) {
+    $('#carousel_' + i).on('slide.bs.carousel', function () {
+      randomEffect($(this).attr("id"), 'slide', 'slide carousel-fade');
+    })
+  };  
+
+  $('#carousel_1').carousel({
+    interval: 6000    
+  });
+  $('#carousel_2').carousel({
+    interval: 5000
+  });
+  $('#carousel_3').carousel({
+    interval: 14000
+  });
+  $('#carousel_4').carousel({
+    interval: 11000
+  });
+  $('#carousel_5').carousel({
+    interval: 9000
+  });
+
+  //$('#product_showcase_main_grid').img
+});
